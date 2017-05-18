ac494fc80b0f8714829db3f4a5e9eec54ea359df
Baek Jonghyeon <iwa15baek@gmail.com>
2017-05-18 14:36:42 +0900

    build and test settings

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Index', () => {
+  it('테스트', () => {
+    assert.ok(false, '첫번째 테스트였습니다.');
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
