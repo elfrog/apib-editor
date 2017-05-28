d6f1ecf7283219d6e6b647e3d6bd1a5dc2df9e39
Baek Jonghyeon <elfrog.jh@gmail.com>
2017-05-28 21:10:50 +0900

    change header property to contain hash header

--- src/parser/model-node.js
+++ src/parser/model-node.js
@@ -1,6 +1,8 @@
 import ApibNode from './apib-node';
 
 export default class ModelNode extends ApibNode {
+  static headerRegex = /^#* (\S+) \(\S+\)$/;
+
   constructor() {
     super();
 
@@ -9,21 +11,21 @@ export default class ModelNode extends ApibNode {
 
   get header() {
     if (this.modelType) {
-      return this.name + ' (' + this.modelType + ')';
+      return this.hashHeader + ' ' + this.name + ' (' + this.modelType + ')';
     }
 
-    return this.name;
+    return super.header;
   }
 
   set header(value) {
-    let result = /(\S+) \(\S+\)/.exec(value);
+    let result = ModelNode.headerRegex.exec(value);
 
     this.name = result[1];
     this.modelType = result[2];
   }
 
   static canAcceptHeader(header) {
-    return /\S+ \(\S+\)/.test(header);
+    return ModelNode.headerRegex.test(header);
   }
 
   checkAcceptableChild(child) {
