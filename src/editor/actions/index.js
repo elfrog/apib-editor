0ba15195ea0c4014073ef65992e2e34ced9149dc
Baek Jonghyeon <iwa15baek@gmail.com>
2017-05-30 18:21:29 +0900

    update actions

--- src/editor/actions/index.js
+++ src/editor/actions/index.js
@@ -1,5 +1,6 @@
 
-import loadNodeList from './load-node-list';
+import openNewDocument from './open-new-document';
+import loadFromFile from './load-from-file';
 import filterNodeList from './filter-node-list';
 import selectNode from './select-node';
 import changeNodeProperty from './change-node-property';
@@ -8,7 +9,8 @@ import removeChildNode from './remove-child-node';
 import changeNodeIndex from './change-node-index';
 
 export let actions = [
-  loadNodeList,
+  openNewDocument,
+  loadFromFile,
   filterNodeList,
   selectNode,
   changeNodeProperty,
