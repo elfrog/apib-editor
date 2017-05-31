228e77d9c8262d45af8761560e95fd13fe0d4982
Baek Jonghyeon <iwa15baek@gmail.com>
2017-05-31 18:30:52 +0900

    check value types automatically

--- src/editor/editor-repository.js
+++ src/editor/editor-repository.js
@@ -3,24 +3,35 @@ export default class EditorRepository {
   static defaultValues = {};
 
   static setItem(key, value) {
-    localStorage.setItem(key, value);
+    if (typeof value === 'object') {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.setItem(key, value);
+    }
   }
 
-  static getItem(key, defaultValue = null) {
+  static getRawItem(key) {
     if (localStorage.hasOwnProperty(key)) {
       return localStorage.getItem(key);
     }
 
-    if (key in EditorRepository.defaultValues) {
-      return EditorRepository.defaultValues[key];
-    }
-
-    return defaultValue;
+    return undefined;
   }
 
-  static getItemAsNumber(key, defaultValue = null) {
-    let value = EditorRepository.getItem(key, defaultValue);
+  static getDefaultValue(key) {
+    return EditorRepository.defaultValues[key];
+  }
 
-    return Number(value);
+  static getItem(key) {
+    let value = EditorRepository.getRawItem(key);
+    let defaultValue = EditorRepository.getDefaultValue(key);
+
+    switch (typeof defaultValue) {
+    case 'string': return value;
+    case 'number': return Number(value);
+    case 'object': return JSON.parse(value);
+    }
+
+    return undefined;
   }
 }
