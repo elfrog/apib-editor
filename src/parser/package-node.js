bdfb3a1fe46c5217c459220665effffd270d4d17
Baek Jonghyeon <iwa15baek@gmail.com>
2017-05-31 10:52:12 +0900

    include ResourceNoe as a possible child node

--- src/parser/package-node.js
+++ src/parser/package-node.js
@@ -1,9 +1,10 @@
 import ApibNode from './apib-node';
 import ResourceGroupNode from './resource-group-node';
 import ModelGroupNode from './model-group-node';
+import ResourceNode from './resource-node';
 
 export default class PackageNode extends ApibNode {
-  static acceptableNodes = [PackageNode, ResourceGroupNode, ModelGroupNode];
+  static acceptableNodes = [PackageNode, ResourceGroupNode, ModelGroupNode, ResourceNode];
 
   static canAcceptHeader(header) {
     return ApibNode.headerRegex.test(header);
