8eeabf7c2c51e8a343fb5dd6e848ea6468404e07
Baek Jonghyeon <iwa15baek@gmail.com>
2017-06-01 15:00:11 +0900

    change menu configuration as json style

--- src/editor/editor-menu.jsx
+++ src/editor/editor-menu.jsx
@@ -3,53 +3,58 @@ import ContextMenu from './components/context-menu';
 
 import { editorCommands as commands }  from './editor-commands';
 
-function openMenu(action, commandItems) {
-  return function (e) {
-    ContextMenu.open({
-      items: commandItems.map(command => makeMenuItem(action, command)),
-      target: e.target
-    });
+const EDITOR_MENU = [
+  {
+    "label": "File",
+    "items": [
+      "newDocument",
+      "openFromFile",
+      null,
+      "downloadFile"
+    ]
+  },
+  {
+    "label": "Edit",
+    "items": [
+      "undo",
+      "redo",
+      null,
+      "showSettingsView"
+    ]
+  },
+  {
+    "label": "Help",
+    "items": [
+      "showStartPage"
+    ]
   }
-}
-
-function makeMenuItem(action, command) {
-  if (command === null) {
-    return { separator: true };
-  }
-
-  return {
-    label: command.label,
-    shortcut: command.shortcut,
-    disabled: command.disabled ? command.disabled(action) : false,
-    onClick: () => {
-      command.onAction(action);
-    }
-  };
-}
+];
 
 export function getMenuItems(action) {
-  return [
-    {
-      label: 'File',
-      onClick: openMenu(action, [
-        commands.newDocument,
-        commands.openFromFile,
-        null,
-        commands.downloadFile
-      ])
-    },
-    {
-      label: 'Edit',
-      onClick: openMenu(action, [
-        commands.undo,
-        commands.redo
-      ])
-    },
-    {
-      label: 'Help',
-      onClick: openMenu(action, [
-        commands.showStartPage
-      ])
-    }
-  ];
+  return EDITOR_MENU.map(menu => {
+    return {
+      label: menu.label,
+      onClick: e => {
+        ContextMenu.open({
+          items: menu.items.map(commandName => {
+            if (commandName === null) {
+              return { separator: true };
+            }
+
+            let command = commands[commandName];
+
+            return {
+              label: command.label,
+              shortcut: command.shortcut,
+              disabled: command.disabled ? command.disabled(action) : false,
+              onClick: () => {
+                command.onAction(action);
+              }
+            };
+          }),
+          target: e.target
+        });
+      }
+    };
+  });
 }
