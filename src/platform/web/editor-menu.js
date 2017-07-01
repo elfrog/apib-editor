b2b1ee39c4216d8e0a7d863298eec83a4a74941e
Jonghyeon Baek <elfrog.jh@gmail.com>
2017-07-01 15:27:59 +0900

    rearrange menu items

--- src/platform/web/editor-menu.js
+++ src/platform/web/editor-menu.js
@@ -1,27 +1,35 @@
+import React from 'react';
+import { MdInsertDriveFile, MdFolderOpen, MdFileDownload, MdUndo, MdRedo, MdHome, MdSettings } from 'react-icons/md';
 
 export const EDITOR_MENU = [
   {
-    "label": "File",
-    "items": [
-      "newDocument",
-      "openFile",
-      null,
-      "saveFile"
-    ]
+    label: <MdInsertDriveFile />,
+    command: 'newDocument' 
   },
   {
-    "label": "Edit",
-    "items": [
-      "undo",
-      "redo",
-      null,
-      "showSettingsView"
-    ]
+    label: <MdFolderOpen />,
+    command: 'openFile'
   },
   {
-    "label": "Help",
-    "items": [
-      "showStartPage"
-    ]
+    label: <MdFileDownload />,
+    command: 'saveFile'
+  },
+  null,
+  {
+    label: <MdUndo />,
+    command: 'undo'
+  },
+  {
+    label: <MdRedo />,
+    command: 'redo'
+  },
+  null,
+  {
+    label: <MdSettings />,
+    command: 'showSettingsView'
+  },
+  {
+    label: <MdHome />,
+    command: 'showStartPage'
   }
 ];
